13fc6343b45bae2325431170d22d7715b5f6bab9
Soukaina El Bouyakoubi <sel-bouy@e2r12p3.1337.ma>
2025-05-03 19:04:30 +0100

    push

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -7,3 +7,23 @@
 Intern::Intern(){}
 
 Intern::Intern(const Intern &copy){}
+
+Intern &Intern::operator=(const Intern &copy){}
+
+Intern::~Intern(){}
+
+AForm* Intern::createPardon(const std::string &target){
+    return new PresidentialPardonForm(target);
+}
+
+AForm* Intern::createRobotomy(const std::string &target){
+    return new RobotomyRequestForm(target);
+}
+
+AForm* Intern::createShrubbery(const std::string &target){
+    return new ShrubberyCreationForm(target);
+}    
+
+AForm* Intern::makeForm(std::string const &formName, std::string const &target){
+           const std::string
+    }
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -5,6 +5,10 @@
 
 class Intern{
 
+    AForm* createShrubbery(const std::string &target);
+    AForm* createRobotomy(const std::string &target);
+    AForm* createPardon(const std::string &target);
+
     public :
         Intern();
         Intern(const Intern &copy);
